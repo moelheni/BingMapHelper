94fd62bff7f45a77977bb0db3455569298d75bfa
insatmc <goalsinhd6@gmail.com>
2014-01-10 21:15:38 -0800

    Adding Files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Bing.Maps;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Xml;
+
+using App13.Map;
+
+using System.Xml.Linq;
+using Windows.ApplicationModel;
+
+// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App13
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            mapHelper map = new mapHelper(BingMap,xText,yText);
+
+            
+
+
+        }
+
+
+        public void GridView1_ItemClick_1(object sender, ItemClickEventArgs e)
+        {
+            rapports r=e.ClickedItem as rapports;
+            string x = r.Title;
+            this.Frame.Navigate(typeof(detailPage),x);
+        }
+
+
+        /// <summary>
+        /// Invoqué lorsque cette page est sur le point d'être affichée dans un frame.
+        /// </summary>
+        /// <param name="e">Données d'événement décrivant la manière dont l'utilisateur a accédé à cette page. La propriété Parameter
+        /// est généralement utilisée pour configurer la page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+      
+    }
+}
---
+++ mapHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Bing.Maps;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace App13.Map
+{
+    class mapHelper
+    {
+        public Bing.Maps.Map map;
+        public double clicked_x;
+        public double clicked_y;
+
+        /* only when map used for forms */
+        public TextBox lat;
+        public TextBox lan;
+
+
+        /* basic use of map to show places */
+        public mapHelper(Bing.Maps.Map m)
+        {
+            map = m;
+        }
+
+        /* map with form, lat = textbox for Latitude and lan = text box for Longitude */
+        public mapHelper(Bing.Maps.Map m,TextBox lat,TextBox lan)
+        {
+            map = m;
+            this.lat = lat;
+            this.lan = lan;
+            map.PointerPressedOverride += MyMap_PointerPressedOverride;
+        }
+
+        private void MyMap_PointerPressedOverride(object sender, PointerRoutedEventArgs e)
+        {
+            map.Children.Clear();
+            Bing.Maps.Location l = new Bing.Maps.Location();
+            this.map.TryPixelToLocation(e.GetCurrentPoint(this.map).Position, out l);
+            Bing.Maps.Pushpin pushpin = new Bing.Maps.Pushpin();
+            pushpin.SetValue(Bing.Maps.MapLayer.PositionProperty, l);
+            map.Children.Add(pushpin);
+
+            clicked_x = l.Latitude;
+            clicked_y = l.Longitude;
+
+            lat.Text = clicked_x.ToString();
+            lan.Text = clicked_y.ToString();
+        }
+
+        public void addPoint(double x, double y)
+        {
+            Pushpin pushpin = new Pushpin();
+            pushpin.Text = "1";
+            MapLayer.SetPosition(pushpin, new Location(x, y));
+            map.Children.Add(pushpin);
+        }
+        
+    }
+}
